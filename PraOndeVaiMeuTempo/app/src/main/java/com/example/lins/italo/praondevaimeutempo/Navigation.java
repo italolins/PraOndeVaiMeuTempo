f342454bf866d4858d110b33c3d22ac5c458d00a
italolins <italo.lins@ccc.ufcg.edu.br>
2016-08-08 09:40:50 -0300

    iniciando UI do app

--- PraOndeVaiMeuTempo/app/src/main/java/com/example/lins/italo/praondevaimeutempo/Navigation.java
+++ PraOndeVaiMeuTempo/app/src/main/java/com/example/lins/italo/praondevaimeutempo/Navigation.java
@@ -1,25 +1,36 @@
 package com.example.lins.italo.praondevaimeutempo;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class Navigation extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_navigation);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);    // <---------- pq isso nao funciona????
+        setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -30,7 +41,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
 
     @Override
     public void onBackPressed() {
@@ -71,18 +81,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            Toast.makeText(getApplicationContext(), "camera",Toast.LENGTH_LONG).show();
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
-            Toast.makeText(this, "gallery",Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_slideshow) {
-            Toast.makeText(this, "slideshow",Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_manage) {
-            Toast.makeText(this, "manage",Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_share) {
-            Toast.makeText(this, "share",Toast.LENGTH_SHORT).show();
+
         } else if (id == R.id.nav_send) {
-            Toast.makeText(this, "send",Toast.LENGTH_SHORT).show();
+
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
